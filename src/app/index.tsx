a8a1a524b265e66e13bedc58c01cd2d154253f95
Guilherme-silva-santos <guilhermesilvasantosjnc@gmail.com>
2024-04-30 09:50:10 -0300

    chrore: validate forms with zod

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,11 +1,32 @@
-import { Image, StatusBar, StyleSheet, View } from "react-native";
+import { Alert, Image, StatusBar, StyleSheet, View } from "react-native";
 import { colors } from "@/styles/colors";
 import { Input } from "../../components/input";
 import { MaterialCommunityIcons } from "@expo/vector-icons";
 import { Button } from "../../components/button";
 import { Link } from "expo-router";
-
+import { useState } from "react";
+import { z } from "zod";
 export default function Home() {
+    const [code, setCode] = useState("")
+    const CodeSchema = z.string()
+    .min(10, { message: "Código inválido o código deve possuir mais de 10 caracteres" })
+    .max(10, { message: "Código inválido o código deve possuir até 10 caracteres" })
+
+    const handleAccessCredential = () => {
+        try {
+            const validatedCode = CodeSchema.parse(code);
+            // continuidade do para quando o campo for envidado ao backend
+            console.log("Código válido:", validatedCode);
+        } catch (err) {
+            if (err instanceof z.ZodError) {
+                Alert.alert("Credencial inválida", err.errors[0].message);
+            } else {
+                console.log("Erro inesperado", err);
+
+            }
+        }
+    }
+
     return (
         <View style={{ backgroundColor: colors.green[500], alignItems: "center", justifyContent: "center", flex: 1, padding: 24 }}>
             <StatusBar barStyle="light-content" />
@@ -15,9 +36,10 @@ export default function Home() {
                     <MaterialCommunityIcons name="ticket-confirmation-outline" size={24} color={colors.green[200]} />
                     <Input.Field
                         placeholder="Código do ingressor"
+                        onChangeText={setCode}
                     />
                 </Input>
-                <Button text="Acessar credencial" isLoading={false} onPress={() => { }} />
+                <Button text="Acessar credencial" isLoading={false} onPress={handleAccessCredential} />
 
                 <Link href="/register" style={styles.link}>
                     Ainda não possui ingresso?
@@ -35,12 +57,12 @@ const styles = StyleSheet.create({
         gap: 18,
     },
 
-    link:{
+    link: {
         color: colors.white,
         fontSize: 16,
         lineHeight: 24,
         textAlign: "center",
         fontWeight: 'bold',
-        marginTop: 10   ,
+        marginTop: 10,
     }
-});
+})
--- src/app/register.tsx
+++ src/app/register.tsx
@@ -1,11 +1,40 @@
-import { Image, StatusBar, StyleSheet, View } from "react-native";
+import { Alert, Image, StatusBar, StyleSheet, View } from "react-native";
 import { colors } from "@/styles/colors";
 import { Input } from "../../components/input";
 import { FontAwesome6, MaterialIcons } from "@expo/vector-icons";
 import { Button } from "../../components/button";
 import { Link } from "expo-router";
+import { useState } from "react";
+import { z } from "zod";
 
 export default function Register() {
+    const [name, setName] = useState("")
+    const [email, setEmail] = useState("")
+
+    const NameSchema = z.string()
+    .min(3, { message: "Nome inválido o nome deve possuir mais de 3 caracteres" })
+    .max(100, { message: "Nome inválido o nome deve possuir até 100 caracteres" })
+    const EmailSchema = z.string()
+    .min(3, { message: "Email inválido o email deve possuir mais de 3 caracteres" })
+    .max(100, { message: "Email inválido o email deve possuir até 100 caracteres" })
+    .email({ message: "Email inválido, por favor, digite um email valido" })
+
+    const handleRegister = () => {
+        try {
+            const validatedName = NameSchema.parse(name);
+            const validatedEmail = EmailSchema.parse(email);
+            // continuidade do para quando o campo for envidado ao backend
+            console.log("Nome válido:", validatedName);
+            console.log("Email válido:", validatedEmail);
+        } catch (err) {
+            if (err instanceof z.ZodError) {
+                Alert.alert("Inscricão inválida", err.errors[0].message);
+            } else {
+                console.log("Erro inesperado", err);
+            }
+        }
+    }
+
     return (
         <View style={{ backgroundColor: colors.green[500], alignItems: "center", justifyContent: "center", flex: 1, padding: 24 }}>
             <StatusBar barStyle="light-content" />
@@ -15,6 +44,7 @@ export default function Register() {
                     <FontAwesome6 name="user-circle" size={20} color={colors.green[200]} />
                     <Input.Field
                         placeholder="Nome Completo"
+                        onChangeText={setName}
                     />
                 </Input>
                 <Input>
@@ -22,9 +52,10 @@ export default function Register() {
                     <Input.Field
                         placeholder="E-mail"
                         keyboardType="email-address"
+                        onChangeText={setEmail}
                     />
                 </Input>
-                <Button text="Realizar inscrição" isLoading={false} onPress={() => { }} />
+                <Button text="Realizar inscrição" isLoading={false} onPress={handleRegister} />
 
                 <Link href="/" style={styles.link}>
                     Já possui ingresso?
