f1f9be3a3c0ad142cbc84a499d095f30bca3ab73
Guilherme-silva-santos <guilhermesilvasantosjnc@gmail.com>
2024-05-08 13:24:09 -0300

    chore: init credential template

--- components/credential.tsx
+++ components/credential.tsx
@@ -1,15 +1,28 @@
-import { colors } from "@/styles/colors";
-import { Image, StyleSheet, Text, View } from "react-native"
+import { colors } from "@/styles/colors"
+import { Image, ImageBackground, StyleSheet, Text, View } from "react-native"
 
 type Props = {
 }
 
-export const Credential = ({}:Props) => {
+export const Credential = ({ }: Props) => {
     return (
         <View style={styles.container}>
             <Image style={styles.band} source={require("@/assets/ticket/band.png")} />
 
-            <View style={styles.blur}></View>
+            <View style={styles.blur}>
+                <ImageBackground style={styles.imgBack} source={require("@/assets/ticket/header.png")}>
+                    <View style={styles.info}>
+                        <Text style={styles.text}>
+                            Unite summit
+                        </Text>
+                        <Text style={styles.text}>
+                            12345678
+                        </Text>
+                    </View>
+                    <View style={styles.userPhotoBlur}/>
+                </ImageBackground>
+                <Image source={{uri: 'https://github.com/guilherme-silva-santos.png'}} style={{width: 140, height: 140, borderRadius: 999, marginTop: -90}} /> 
+            </View>
         </View>
     )
 }
@@ -17,16 +30,16 @@ export const Credential = ({}:Props) => {
 const styles = StyleSheet.create({
     container: {
         width: "100%",
-       alignSelf: "stretch",
-       alignItems: "center",
+        alignSelf: "stretch",
+        alignItems: "center",
     },
-    band:{
+    band: {
         width: 96,
         height: 208,
         zIndex: 10,
-    }, 
-    blur:{
-        backgroundColor: 'rgba(0, 0, 0, 0.2)',  
+    },
+    blur: {
+        backgroundColor: 'rgba(0, 0, 0, 0.2)',
         alignSelf: "stretch",
         alignItems: "center",
         paddingBottom: 24,
@@ -36,5 +49,33 @@ const styles = StyleSheet.create({
         marginRight: 12,
         borderRadius: 8,
         marginTop: -10,
+    },
+    imgBack: {
+        paddingHorizontal: 24,
+        paddingVertical: 32,
+        height: 160,
+        alignItems: "center",
+        alignSelf: "stretch",
+        borderBottomWidth: 1,
+        borderBottomColor: 'rgba(255, 255, 255, 0.1)',
+        overflow: "hidden",
+    },
+    info:{
+        width: "100%",
+        flexDirection: 'row',
+        justifyContent: "space-between",
+        alignItems: "center",
+    },
+    text:{
+        color: colors.gray[200],
+        fontSize: 16,
+        lineHeight: 24,
+        fontWeight: 'bold',
+    },
+    userPhotoBlur:{
+        width: 160,
+        height: 160,
+        backgroundColor: 'black',
+        borderRadius: 999,
     }
 })
